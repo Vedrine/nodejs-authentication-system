6434fe67cc814105483d62e493c9e6523c390189
svedrine <sebastien94170@gmail.com>
2020-10-05 20:30:14 +0200

    Routes + controllers + services setup

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const routes = require('./app/routes');
+const routes = require('./app/api/routes');
+const bodyParser = require('body-parser');
 
 // SERVER
 const hostname = 'localhost';
@@ -9,11 +10,21 @@ const port = 3000;
 const app = express();
 const server = http.createServer(app);
 
+// PARSE APPLICATION/X-WWW-FORM-URLENCODED
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// PARSE APPLICATION/JSON
+app.use(bodyParser.json());
+
 // STATIC FOLDER
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 // ROUTES
 app.use('/', routes);
+
+// VIEW ENGINE
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'ejs');
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
